c50dee2e9738353ed3fc58ea52192495475e66d1
MarcosGuzman0077 <thedreamer5757@gmail.com>
2025-05-30 14:58:41 -0400

    Add files via upload

---
+++ NumeroController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Actividad2Unidad1.Models;
+
+namespace Actividad2Unidad1.Controllers
+{
+    public class NumeroController : Controller
+    {
+        // GET: Numero
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Verificar(NumeroModel model)
+        {
+            if (EsPrimo(model.Numero))
+            {
+                model.Mensaje = $"El número {model.Numero} es primo.";
+            }
+            else
+            {
+                model.Mensaje = $"El número {model.Numero} no es primo.";
+            }
+
+            return View("Index", model);
+        }
+
+        private bool EsPrimo(int numero)
+        {
+            if (numero <= 1) return false;
+            for (int i = 2; i <= System.Math.Sqrt(numero); i++)
+            {
+                if (numero % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
+    
