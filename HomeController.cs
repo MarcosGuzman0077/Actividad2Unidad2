aeb64ee8d44c8f94577ccd7b18ba20ed7a2416e2
MarcosGuzman0077 <thedreamer5757@gmail.com>
2025-05-30 14:57:22 -0400

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Actividad2Unidad1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Saludo(string nombre)
+        {
+            ViewBag.Mensaje = "Hola, " + nombre + ", bienvenido a ASP.NET MVC.";
+            return View("Index");
+        }
+    }
+}
