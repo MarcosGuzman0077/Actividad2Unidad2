fc8fcf64f1cb0063eab0560b1d1616ddafd20e4a
MarcosGuzman0077 <thedreamer5757@gmail.com>
2025-05-30 14:56:51 -0400

    Add files via upload

---
+++ EvaluacionController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Actividad2Unidad1.Models;
+
+namespace Actividad2Unidad1.Controllers
+{
+    public class EvaluacionController : Controller
+    {
+        // GET: Evaluacion
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Calcular(EvaluacionModel model)
+        {
+            model.Promedio = (model.Calificacion1 + model.Calificacion2 + model.Calificacion3 + model.Calificacion4) / 4.0;
+
+            if (model.Promedio >= 70)
+                model.Estado = "Aprobado";
+            else if (model.Promedio >= 60)
+                model.Estado = "En recuperación";
+            else
+                model.Estado = "Reprobado";
+
+            return View("Index", model);
+        }
+    }
+}
