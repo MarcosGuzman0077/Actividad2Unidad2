c15cf032fb833d6e8e5c2358d563983d3c23bf69
MarcosGuzman0077 <thedreamer5757@gmail.com>
2025-05-30 14:57:55 -0400

    Add files via upload

---
+++ LoginController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Actividad2Unidad1.Models;
+
+namespace Actividad2Unidad1.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginModel model)
+        {
+            if (model.Usuario == "admin" && model.Contrasena == "123456")
+            {
+                return RedirectToAction("Bienvenido");
+            }
+            else
+            {
+                model.Mensaje = "Usuario incorrecto";
+                return View(model);
+            }
+        }
+
+        public ActionResult Bienvenido()
+        {
+            return Content("Bienvenido");
+        }
+    }
+}
