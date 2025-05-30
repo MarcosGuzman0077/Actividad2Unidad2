8a8bd585a75729ec33997376c8c20bee7bd4cee3
MarcosGuzman0077 <thedreamer5757@gmail.com>
2025-05-30 14:56:16 -0400

    Add files via upload

---
+++ CalculadoraController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Actividad2Unidad1.Models;
+
+namespace Actividad2Unidad1.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        // GET: Calculadora
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Calcular(OperacionModel model)
+        {
+            switch (model.Operacion)
+            {
+                case "+":
+                    model.Resultado = model.Numero1 + model.Numero2;
+                    break;
+                case "-":
+                    model.Resultado = model.Numero1 - model.Numero2;
+                    break;
+                case "*":
+                    model.Resultado = model.Numero1 * model.Numero2;
+                    break;
+                case "/":
+                    model.Resultado = model.Numero2 != 0 ? model.Numero1 / model.Numero2 : 0;
+                    break;
+                default:
+                    ModelState.AddModelError("", "Operación no válida");
+                    break;
+            }
+
+            return View("Index", model);
+        }
+    }
+}
